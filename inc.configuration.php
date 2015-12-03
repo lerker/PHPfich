36ba1763435a5a89587f7be696e5957c354f7cca
Nelson Ponzoni <npcuadra@gmail.com>
2015-12-03 17:16:35 -0300

    ads

--- inc.configuration.php
+++
@@ -1,9 +0,0 @@
-<?php
-	ini_set("display_errors",1); 
-	error_reporting(E_ALL);
-	
-	$config["db"] = "cine";
-	$config["dbuser"] = "root";
-	$config["dbpass"] = "";
-	$config["dbhost"]="localhost";
-	$config["dbEngine"]="MYSQL";
