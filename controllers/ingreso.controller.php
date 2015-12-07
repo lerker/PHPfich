41e9ea76fb86461853ebd0ef1a2d85668ded1eb1
Nelson Ponzoni <npcuadra@gmail.com>
2015-12-07 09:52:04 -0300

    arreglado el index

--- controllers/ingreso.controller.php
+++ controllers/ingreso.controller.php
@@ -9,17 +9,12 @@ class Ingreso_Controller {
     	$tpl->prepare();
     	$tpl->gotoBlock("_ROOT");	# desde el comienzo
 
+        //----------------------------------
+        //      LISTADOS
+        //
     	$tpl->newBlock("blockItemMenu");
     	$tpl->assign("var_link_direccion", "index.php?action=Pelicula::listadoPeliculas");
 		$tpl->assign("var_link_mostrar", "Listado de Peliculas");
-
-		$tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Pelicula::peliculasPorGenero");
-		$tpl->assign("var_link_mostrar", "Listado de Peliculas por Genero");
-
-		$tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Director::peliculasPorDirector");
-		$tpl->assign("var_link_mostrar", "Listado de Peliculas por Director");
 
 		$tpl->newBlock("blockItemMenu");
 		$tpl->assign("var_link_direccion", "index.php?action=Artista::listadoArtistas");
@@ -33,43 +28,42 @@ class Ingreso_Controller {
 		$tpl->assign("var_link_direccion", "index.php?action=Director::listadoDirectores");
 		$tpl->assign("var_link_mostrar", "Listado de Directores");
 
-
-		$tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Actor::listadoActoresPorPelicula");
-		$tpl->assign("var_link_mostrar", "Listado de Actores por Pelicula");
-
 		$tpl->newBlock("blockItemMenu");
 		$tpl->assign("var_link_direccion", "index.php?action=Genero::listadoGeneros");
 		$tpl->assign("var_link_mostrar", "Listado de Generos");
 
-		$tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Pelicula::eliminarPelicula");
-		$tpl->assign("var_link_mostrar", "Eliminar una Pelicula");
+        $tpl->newBlock("blockItemMenu");
+        $tpl->assign("var_link_direccion", "index.php?action=Sala::listarSalas");
+        $tpl->assign("var_link_mostrar", "Listado de Salas");
 
-		// simplemente una prueba de agregar otro
+        //-----------------------------
+        //      ABM
+        //
+
 		$tpl->newBlock("blockItemMenu");
 		$tpl->assign("var_link_direccion", "index.php?action=Pelicula::altaPelicula");
 		$tpl->assign("var_link_mostrar", "Alta Pel&iacute;cula");
-
-		$tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Actor::altaActor");
-		$tpl->assign("var_link_mostrar", "Alta Actor");
-
-        $tpl->newBlock("blockItemMenu");
-		$tpl->assign("var_link_direccion", "index.php?action=Director::altaDirector");
-		$tpl->assign("var_link_mostrar", "Alta Director");
 
         $tpl->newBlock("blockItemMenu");
         $tpl->assign("var_link_direccion", "index.php?action=Artista::AltaArtista");
         $tpl->assign("var_link_mostrar", "Alta Artista");
 
         $tpl->newBlock("blockItemMenu");
-        $tpl->assign("var_link_direccion", "index.php?action=Sala::listarSalas");
-        $tpl->assign("var_link_mostrar", "Listado de Salas");
+        $tpl->assign("var_link_direccion", "index.php?action=Actor::altaActor");
+        $tpl->assign("var_link_mostrar", "Alta Actor");
+
+        $tpl->newBlock("blockItemMenu");
+        $tpl->assign("var_link_direccion", "index.php?action=Director::altaDirector");
+        $tpl->assign("var_link_mostrar", "Alta Director");
 
         $tpl->newBlock("blockItemMenu");
         $tpl->assign("var_link_direccion", "index.php?action=Sala::altaSala");
         $tpl->assign("var_link_mostrar", "Alta de Sala");
+
+        $tpl->newBlock("blockItemMenu");
+        $tpl->assign("var_link_direccion", "index.php?action=Genero::altaGenero");
+        $tpl->assign("var_link_mostrar", "Alta de Genero");
+
 
 
     return $tpl->getOutputContent();
